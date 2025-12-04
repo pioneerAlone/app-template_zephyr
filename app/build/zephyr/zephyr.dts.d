e285c28064e4ca3d416bd677d3b193abca4a9157
bakewell <slkybowang@gmailc.om>
2025-12-04 09:49:50 +0800

    Remove build directories from tracking and update .gitignore

--- app/build/zephyr/zephyr.dts.d
+++
@@ -1,8 +0,0 @@
-empty_file.o: \
- /Users/wangbo/zephyrproject/external/zephyr/misc/empty_file.c \
- /Users/wangbo/zephyrproject/external/zephyr/boards/qemu/x86/qemu_x86.dts \
- /Users/wangbo/zephyrproject/external/zephyr/dts/common/mem.h \
- /Users/wangbo/zephyrproject/external/zephyr/dts/x86/intel/atom.dtsi \
- /Users/wangbo/zephyrproject/external/zephyr/dts/common/skeleton.dtsi \
- /Users/wangbo/zephyrproject/external/zephyr/include/zephyr/dt-bindings/interrupt-controller/intel-ioapic.h \
- /Users/wangbo/zephyrproject/external/zephyr/include/zephyr/dt-bindings/pcie/pcie.h
